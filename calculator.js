eab65cf60355f8b5086d61c4f03a79616a4bbae5
Ramya2010 <47886253+Ramya2010@users.noreply.github.com>
2021-03-04 12:44:38 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded(extended : true));
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname) ;
+});
+
+app.post("/", function(req, res)
+{
+
+  var num1=Number(req.body.n1);
+  var num2=Number(req.body.n2);
+
+  var result = num1 + num2;
+  res.send("The result Calculation is" + result);
+});
+
+app.get("/bmiCalculator", function(req, res)
+{
+  res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.listen(3000, function(){
+  console.log("server started on port 3000");
+});
